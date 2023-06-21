af656ae68e113f0d084699d02560a574685889d3
slbabin <>
2023-06-21 21:55:09 +0000

    Create variables and printng them out

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 word = "test"
 current_word = "-" * len(word)
 print(f"The chosen word is: {current_word}")
+mistakes = 0
+letters = ""
+
+while not(word == current_word or mistakes > 6):
+    print(f"Word: {current_word}")
+    print(f"Mistakes: {mistakes}")
+    print(f"Added letters: ", end="")
+    if len(letters) == 0:
+        print("-")
+    else:
+        print(*letters)
+    new_letter = input("Enter a letter: ").lower()
+    
+
