943634afee0340b3fac683f7dd3d561842268bcc
slbabin <>
2023-06-21 21:21:27 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+word = "test"
+current_word = "-" * len(word)
+print(f"The chosen word is: {current_word}")
