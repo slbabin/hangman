fe8ac44cc31ff724e851cbb94f0ac220f7a0e2f7
slbabin <>
2023-06-22 22:44:15 +0000

    Add statement to ask if the user want to play again. Add outer whil loop.

--- run.py
+++ run.py
@@ -1,49 +1,67 @@
 ALPHABET = "ABDEFGHIJKLMNOPQRSTUVWXYZ"
 
-word = "SUPER"
-current_word = "-" * len(word)
-print(f"The chosen word is: {current_word}\n")
-mistakes = 0
-letters = ""
+want_to_play = True
 
-while not (word == current_word or mistakes > 6):
-    print(f"Word: {current_word}")
-    print(f"Mistakes: {mistakes} out of 7")
-    print(f"Added letters: ", end="")
+while want_to_play:
+    word = "SUPER"
+    current_word = "-" * len(word)
+    print(f"The chosen word is: {current_word}\n")
+    mistakes = 0
+    letters = ""
     
-
-    if len(letters) == 0:
-        print("-")
-    else:
-        print(*letters)
-    new_letter = input("Enter a letter: ").upper()
-
-    while not (len(new_letter) == 1 and new_letter in ALPHABET):
-
-        new_letter = input("Input only one latin letter: ").upper()
+    while not (word == current_word or mistakes > 6):
+        print(f"Word: {current_word}")
+        print(f"Mistakes: {mistakes} out of 7")
+        print(f"Added letters: ", end="")
         
-    print()
-    letter_in_word = False
 
-    for i in range(len(word)):
-        
-        if new_letter == word[i]:  # checking if a letter exists in the word
-            current_word = current_word[:i] + new_letter + current_word[i + 1:]
-            letter_in_word = True
+        if len(letters) == 0:
+            print("-")
+        else:
+            print(*letters)
+        new_letter = input("Enter a letter: ").upper()
 
-    if not letter_in_word:
-        mistakes += 1
-    if not new_letter in letters:
-        letters += new_letter
+        while not (len(new_letter) == 1 and new_letter in ALPHABET):
+
+            new_letter = input("Input only one latin letter: ").upper()
+            
+        print()
+        letter_in_word = False
+
+        for i in range(len(word)):
+            
+            if new_letter == word[i]:  # checking if a letter exists in the word
+                current_word = current_word[:i] + new_letter + current_word[i + 1:]
+                letter_in_word = True
+
+        if not letter_in_word:
+            mistakes += 1
+        if not new_letter in letters:
+            letters += new_letter
 
 print()
 print(f"Word: {current_word}")
 print(f"Mistakes: {mistakes} out of 7")
 print(f"Added letters: ", end="")
 
+if len(letters) == 0:
+    print("-")
+else:
+    print(*letters)
+
 if word == current_word:
     print("Congratulations! You guessed the word!")
 else:
     print("Sorry, but didn't guess the word.")    
 
-print(f"The correct word is: {word}")    
+print(f"The correct word is: {word}") 
+
+
+play_again = input("Do you want to play again?")
+while not(play_again == "yes" or again == "no"):
+    play_again = input("Just enter \"Yes\" or \"No\": ").lower()
+if play_again == "no":
+    want_to_play = False
+
+print("Thank you for your game!")    
+input("Click Enter to finish the game.")
